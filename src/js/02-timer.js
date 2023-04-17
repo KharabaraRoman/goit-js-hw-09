443d41cb81ead51fbb834e430c436bcb5b4a0e60
Роман Харабара <117296212+KharabaraRoman@users.noreply.github.com>
2023-04-17 19:36:17 +0200

    Ок

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,15 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from "notiflix";
+import Notiflix from 'notiflix';
 
-   const datetimePicker = document.querySelector("#datetime-picker");
-const startBtn = document.querySelector("#start-btn");
-const daysValue = document.querySelector("[data-days]");
-const hoursValue = document.querySelector("[data-hours]");
-const minutesValue = document.querySelector("[data-minutes]");
-const secondsValue = document.querySelector("[data-seconds]");
+const startBtn = document.querySelector('[data-start]');
+const datetimePicker = document.querySelector('#datetime-picker');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
 
-let countdownInterval;
+startBtn.disabled = true;
 
 const options = {
   enableTime: true,
@@ -18,40 +18,40 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     const selectedDate = selectedDates[0];
-    const now = new Date();
-    if (selectedDate < now) {
-      window.alert("Please choose a date in the future");
+    const currentDate = new Date();
+    if (selectedDate <= currentDate) {
+      Notiflix.Notify.failure("Please choose a date in the future");
       startBtn.disabled = true;
     } else {
-      startBtn.disabled = false;
+        startBtn.disabled = false;
     }
+    console.log(selectedDates[0]);
   },
 };
 
-flatpickr(datetimePicker, options);
+flatpickr("#datetime-picker", options);
 
-startBtn.addEventListener("click", () => {
-  const selectedDate = new Date(datetimePicker.value);
-  clearInterval(countdownInterval);
-  countdownInterval = setInterval(() => {
-    const now = new Date();
-    const timeDifference = selectedDate - now;
+startBtn.addEventListener('click', onStartBtn);
 
-    if (timeDifference < 0) {
-      clearInterval(countdownInterval);
-      return;
+function onStartBtn () {
+    const selectedDate = new Date(datetimePicker.value);
+    const timerInterval = setInterval(updateTimer, 1000);
+    startBtn.disabled = true;
+ 
+    function updateTimer() {
+        const currentDate = new Date();
+        const timeDifference = selectedDate - currentDate;
+        if (timeDifference <= 0) {
+            clearInterval(timerInterval);
+            return;
+        }
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        daysElement.textContent = String(days).padStart(2, '0');
+        hoursElement.textContent = String(hours).padStart(2, '0');
+        minutesElement.textContent = String(minutes).padStart(2, '0');
+        secondsElement.textContent = String(seconds).padStart(2, '0');
     }
-
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (timeDifference / (1000 * 60 * 60)) % 24
-    );
-    const minutes = Math.floor((timeDifference / 1000 / 60) % 60);
-    const seconds = Math.floor((timeDifference / 1000) % 60);
-
-    daysValue.textContent = days.toString().padStart(2, "0");
-    hoursValue.textContent = hours.toString().padStart(2, "0");
-    minutesValue.textContent = minutes.toString().padStart(2, "0");
-    secondsValue.textContent = seconds.toString().padStart(2, "0");
-  }, 1000);
-});
+}
